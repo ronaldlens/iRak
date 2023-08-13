8247990f5f3da9c0c398c680ece068889f1850e9
Ronald Lens <ronald@ronaldlens.com>
2023-08-13 17:42:09 +0200

    add buttons, annotation instead of marker

---
+++ iRak/BottomButtons.swift
@@ -0,0 +1,18 @@
+//
+//  BottomButtons.swift
+//  iRak
+//
+//  Created by Ronald Lens on 13/08/2023.
+//
+
+import SwiftUI
+
+struct BottomButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomButtons()
+}
